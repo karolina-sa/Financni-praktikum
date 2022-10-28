196b645580e2bfe417a7c78bd396371c77bc2c1b
Karolina Savli <karolina.savli@gmail.com>
2022-10-28 10:49:22 +0200

    Okolje za 2. domačo nalogo

---
+++ 2. DOMAČA NALOGA/Savli2.r
@@ -0,0 +1,12 @@
+# 2. DOMAČA NALOGA: VREDNOTENJE EKSOTIČNIH OPCIJ
+# Finančni praktikum 2022/23
+# Karolina Šavli
+
+# 1. naloga ####################################################################
+# a)
+S0 <- 50
+u <- 1.05
+d <- 0.95
+U <- 5
+R <- 0.03 # oz. 3 %
+T <- 3
